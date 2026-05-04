f2b068d953a89800c2ce78fa69b660046e27390a
KhandlaMayur <khandlamayur62@gmail.com>
2026-05-04 12:24:27 +0530

    upload Procfile and requirements.txt file

--- app.py
+++ app.py
@@ -257,4 +257,6 @@ def predict():
 # RUN SERVER
 # =========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("DEBUG", "false").lower() == "true"
+    app.run(host="0.0.0.0", port=port, debug=debug)
